9bacc3aad5af9ed4c7350d454e551c0778df14a3
●brayan● ●Cordoba● <brayancordoba242@gmail.com>
2023-08-30 19:33:12 -0500

    Eliminar tarea segun indice en array

--- maqueta/script.js
+++ maqueta/script.js
@@ -1,57 +1,65 @@
-const todoList = document.querySelectorAll('.todo-list');
+const todoList = document.querySelector('.todo-list');
 const input = document.querySelector('.new-todo')
 
-const tareas = []
-// const footer = document.querySelector('.footer');
-// const toogle = document.querySelectorAll('.toogle');
-// const deleteBtn = document.getElementById('borrarBtn')
-// const editBtn = document.querySelectorAll('.editBtn');
-// const updateController = document.querySelectorAll('.update-controller');
-// const textTarea = document.querySelectorAll('.input-controller textarea')
-// const editControllerSelect = document.querySelectorAll('.edit-controller select')[0];
-// const priority = document.querySelectorAll('#priority')
-// const editControllerSelect1 = document.querySelectorAll('.edit-controller select')[1];
-// const category = document.querySelectorAll('#category');
-// const saveBtn = document.querySelectorAll('.saveBtn');
-// const cancelBtn = document.querySelectorAll('.cancelBtn');
-function createTask(taskText) {
-  const taskElement = document.createElement('li');
+let tareas = []
+function eliminar(index)
+{
+  const tareasFiltradas = tareas.filter((_, i) => i !== index)
 
-  const checkbox = document.createElement('input');
-  checkbox.type = 'checkbox';
-  checkbox.classList.add('task-checkbox');
+  tareas = [...tareasFiltradas]
+  render()
+}
 
-  taskElement.appendChild(checkbox);
+function render() {
+  while (todoList.children.length > 0)
+  {
+    todoList.lastChild.remove()
+  }
 
-  taskElement.appendChild(document.createTextNode(taskText));
+  tareas.forEach((e, i) => {
+    const taskElement = document.createElement('li');
 
-  const todoList = document.querySelector('.todo-list');
-  todoList.appendChild(taskElement);
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.classList.add('task-checkbox');
 
-  const buttonEdit = document.createElement('button');
-  const iconEdit = document.createElement('img');
-  iconEdit.classList.add('icon-edit');
-  buttonEdit.classList.add('button');
-  iconEdit.setAttribute('src', 'pencil-square.svg');
+    taskElement.appendChild(checkbox);
 
-  const buttonBorrar = document.createElement('button');
-  const iconBorrar = document.createElement('img');
-  iconBorrar.classList.add('icon-borrar');
-  buttonBorrar.classList.add('button', 'button-delete');
-  iconBorrar.setAttribute('src', 'trash-fill.svg');
+    taskElement.appendChild(document.createTextNode(e));
 
-  buttonEdit.appendChild(iconEdit);
-  taskElement.appendChild(buttonEdit);
-  buttonBorrar.appendChild(iconBorrar);
-  taskElement.appendChild(buttonBorrar);
+    const todoList = document.querySelector('.todo-list');
+    todoList.appendChild(taskElement);
+
+    const buttonEdit = document.createElement('button');
+    const iconEdit = document.createElement('img');
+    iconEdit.classList.add('icon-edit');
+    buttonEdit.classList.add('button');
+    iconEdit.setAttribute('src', 'pencil-square.svg');
+
+    const buttonBorrar = document.createElement('button');
+    const iconBorrar = document.createElement('img');
+    iconBorrar.classList.add('icon-borrar');
+    buttonBorrar.classList.add('button', 'button-delete');
+    iconBorrar.setAttribute('src', 'trash-fill.svg');
+
+    buttonBorrar.addEventListener('click', () => {
+      // alert('eliminar ' + i)
+      eliminar(i)
+    })
+
+    buttonEdit.appendChild(iconEdit);
+    taskElement.appendChild(buttonEdit);
+    buttonBorrar.appendChild(iconBorrar);
+    taskElement.appendChild(buttonBorrar);
+  })
 }
 
 input.addEventListener('keyup', (event) => {
   if (event.key === 'Enter' && event.target.value.length > 0) {
     const taskText = event.target.value;
-    createTask(taskText);
-    event.target.value = '';
     tareas.push(taskText)
+    render()
+    event.target.value = '';
     console.log(tareas)
   }
 });
